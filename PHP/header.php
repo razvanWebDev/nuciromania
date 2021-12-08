c74df5405172bfcc30a6425ede92589c2dd8ec55
razvanWebDev <crsn_razvan@yahoo.com>
2021-12-08 19:02:04 +0200

    added meta description and keywords

--- PHP/header.php
+++ PHP/header.php
@@ -8,6 +8,10 @@
     <meta charset="UTF-8" lang="RO-ro">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description"
+        content="În 2021, am decis înființarea unui centru de colectare și procesare nucă, dedicat producătorilor din România, care să ofere certitudinea valorificării producției rezultate din suprafețele de cultură mici și medii, precum și a nucii provenite din flora spontană sau nuci individuali.">
+    <meta name="keywords"
+        content="nuca Romania, cumpar nuca Romania, vand nuca Romania, nuci Romania, achizitie nuca Romania, pret miez nuca Romania, procesare nuca Romania">
     <link rel="stylesheet" href="css/style.css">
     <title>Nuci Romania</title>
     <script src="https://www.google.com/recaptcha/api.js?render=<?php echo $site_key ?>"></script>
